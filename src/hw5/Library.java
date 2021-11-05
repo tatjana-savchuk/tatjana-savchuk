1d10d62fb1a28f6679d641c56002d040cda1f4ae
tatjana savchuk <tatianawisa@gmail.com>
2021-11-05 21:54:08 +0200

    init commit

--- src/hw5/Library.java
+++ src/hw5/Library.java
@@ -1,6 +1,7 @@
 package hw5;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 //Выводит на консоль сообщение
 //        "Петров В. В. взял книги: Приключения (Иванов И. И. 2000 г.),
@@ -14,7 +15,40 @@ import java.util.ArrayList;
 //        Словарь (Сидоров А. В 1980 г.), Энциклопедия (Гусев К. В. 2010 г.)".
 class Library {
     public static void main(String[] args) {
-        System.out.println();
+
+        Book[] bookArray = new Book[3];
+        bookArray[0] = new Book( "Приключения", "Иванов И.И", "2010" );
+        bookArray[1] = new Book( "Словарь", "Петров И.И", "2001" );
+        bookArray[2] = new Book( "Энциклопедия", "Сидоров И.И", "2005" );
+
+        for (int i = 0; i < bookArray.length; i++) {
+            System.out.println( bookArray[i] );
+        }
+
+        Reader[] readerArray = new Reader[3];
+        readerArray[0] = new Reader( "Петров В.В", "Marketing", "5 мая 1993",
+                "001", "0509298531" );
+        readerArray[1] = new Reader( "Старшов В.В", "economic", "1 января 1995", "002", "099707080" );
+        readerArray[2] = new Reader( "Гусев В.В", "it", "3 марта 1990", "003", "09812345678" );
+
+        for (int i = 0; i < readerArray.length; i++) {
+            System.out.println( readerArray[i] );
+        }
+
+//        private static void printBooks (bookArray) {
+//            System.out.println("Список книг:");
+//            for (Book book : bookArray) {
+//                System.out.println(book);
+//            }
+            readerArray [0].takeBook( "Приключения", "Словарь" );
+        readerArray[1].takeBook( "Энциклопедия", "Сидоров" );
+        readerArray[2].returnBook("Приключения");
+
+
+
+
+
+
 
 
 
--- src/hw5/Reader.java
+++ src/hw5/Reader.java
@@ -1,91 +1,141 @@
 package hw5;
 
 public class Reader {
-    String name;
-    int numberTicket;
-    String fac;
-    String dateBirthday;
-    String phone;
 
-    public static void main(String[] args) {
+    private String name;
+    private String numberTicket;
+    private String fac;
+    private String dateBirthday;
+    private String phone;
+    int bookCount =0;
 
-        String reader1 = "Петров В.В";
-        String reader2 = "Старшов В.В";
-        String reader3 = "Гусев В.В";
+    public String getName() {
+        return name;
+    }
 
-        String array[] = new String[3];
-        String[] readers = {reader1, reader2, reader3};
-        for (int i = 0; i < array.length; i++) {
-            System.out.println( readers[i] );
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNumberTicket() {
+        return numberTicket;
+    }
+
+    public void setNumberTicket(String numberTicket) {
+        this.numberTicket = numberTicket;
+    }
+
+    public String getFac() {
+        return fac;
+    }
+
+    public void setFac(String fac) {
+        this.fac = fac;
+    }
+
+    public String getDateBirthday() {
+        return dateBirthday;
+    }
+
+    public void setDateBirthday(String dateBirthday) {
+        this.dateBirthday = dateBirthday;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public int getBookCount() {
+        return bookCount;
+    }
+
+    public void setBookCount(int bookCount) {
+        this.bookCount = bookCount;
+    }
+
+    public Reader (String name, String fac, String dateBirthday, String  numberTicket, String phone) {
+            this.name = name;
+            this.fac = fac;
+            this.dateBirthday = dateBirthday;
+            this.numberTicket = numberTicket;
+            this.phone = phone;
+
         }
 
 
-//
-//    public String getName() {
-//        return name;
-//    }
-//
-//    public void setName(String name) {
-//        this.name = name;
-//    }
-//
-//    public int getNumberTicket() {
-//        return numberTicket;
-//    }
-//
-//    public void setNumberTicket(int numberTicket) {
-//        this.numberTicket = numberTicket;
-//    }
-//
-//    public String getFac() {
-//        return fac;
-//    }
-//
-//    public void setFac(String fac) {
-//        this.fac = fac;
-//    }
-//
-//    public String getDateBirthday() {
-//        return dateBirthday;
-//    }
-//
-//    public void setDateBirthday(String dateBirthday) {
-//        this.dateBirthday = dateBirthday;
-//    }
-//
-//    public String getPhone() {
-//        return phone;
-//    }
-//
-//    public void setPhone(String phone) {
-//        this.phone = phone;
-//    }
-//
-//    public static void main(String[] args) {
-//        Reader reader1 = new Reader();
-//        Reader reader2 = new Reader();
-//        Reader reader3 = new Reader();
-//        reader1.name = "Петров В.В";
-//        reader2.name = "Старшов В.В";
-//        reader3.name = "Гусев В.В";
-//        reader1.fac = "merketing";
-//        reader2.fac = "economic";
-//        reader1.dateBirthday = "5 мая 1993";
-//        reader2.dateBirthday = "1 января 1995";
-//        reader1.numberTicket = 58965;
-//        reader2.numberTicket = 74156;
-//        reader1.phone = "+380509298531";
-//        reader2.phone = "+380997080500";
-//        System.out.println( reader1.getName());
-//        reader1.takeBook();
-//
-//
-//
-//    }
-//    public void takeBook ()
-//    {
-//        System.out.println( "взял 3 книги");
+    public void takeBook(String... books) {
+        System.out.println(this.name + " взял следующие книги:");
+        Book[] bookArray = new Book[3];
+        bookArray[0] = new Book( "Приключения", "Иванов И.И", "2010" );
+        bookArray[1] = new Book( "Словарь", "Петров И.И", "2001" );
+        bookArray[2] = new Book( "Энциклопедия", "Сидоров И.И", "2005" );
+        for (Book book : bookArray) {
+            System.out.println(book);
+        }
+        System.out.println();
+    }
+
+    public void takeBook(Book... books) {
+        System.out.println(this.name + " взял следующие книги:");
+        for (Book book : books) {
+            System.out.println(book.getNameBook() + ", автор - " + book.getAvtor());
+        }
+        System.out.println();
+    }
+
+
+    public void returnBook(String... books) {
+        System.out.println(this.name + " вернул следующие книги:");
+        for (String book : books) {
+            System.out.println(book);
+        }
+        System.out.println();
+    }
+
+//    public void returnBook(Book... books) {
+//        System.out.println(this.name + " вернул следующие книги:");
+//        for (Book book : books) {
+//            System.out.println( book.getNameBook() + ", автор - " + book.getAvtor() );
+//        }
 //    }
 
+    @Override
+    public String toString() {
+        return this.name + " numberTicket -" + this.numberTicket + "fac ,"+ this.fac +" dateBirthday ,"+ this.dateBirthday +" , phone +38"+ this.phone;
     }
-}
+
+
+    public static void main(String[] args) {
+        Reader[] readerArray = new Reader[3];
+        readerArray[0] = new Reader("Петров В.В","Marketing","5 мая 1993",
+                "001","0509298531");
+        readerArray[1] = new Reader("Старшов В.В","economic","1 января 1995","002","099707080");
+        readerArray[2] = new Reader("Гусев В.В","it","3 марта 1990","003","09812345678");
+
+        for (int i = 0; i < readerArray.length; i++) {
+            System.out.println( readerArray[i] );
+        }
+
+
+
+    }
+
+    private class ToStringBuilder {
+        public ToStringBuilder(Reader reader) {
+        }
+
+        public ToStringBuilder append(String name, String nanumberTicketme1  ) {
+            return null;
+        }
+
+
+
+
+
+    }
+
+    }
